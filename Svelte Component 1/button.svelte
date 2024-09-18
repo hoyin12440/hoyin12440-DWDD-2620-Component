89d481eb7eada816bd1fce5aaa6bad0ccd6c2b3f
hoyin12440 <119979465+hoyin12440@users.noreply.github.com>
2024-09-18 12:45:59 -0600

    added changes
    added changes

---
+++ Svelte Component 1/button.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name = "Default";
+	export let type = "regular";
+</script>
+
+
+{#if type=="regular"}
+	<button class="bg-white hover:bg-black px-10 py-2 rounded-3xl">{name}</button>
+{/if}
+
+{#if type=="disabled"}
+	<button class="bg-black hover:bg-white px-10 py-2 rounded-3xl">{name}</button>
+{/if}
---
+++ Svelte Component 1/index.svelte
@@ -0,0 +1,9 @@
+<script>
+	import MyButton from "./button.svelte";
+</script>
+
+<svelte:head>
+<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+</svelte:head>
+
+<MyButton name="Login" type="regular"/>
